be07bae4094bad471467a342f26f01538d420448
gv4383 <gregory.vargas18@gmail.com>
2022-09-22 17:39:32 -0500

    Project setup

---
+++ ProtegeTakeHome/Screens/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  ProtegeTakeHome
+//
+//  Created by Goyo Vargas on 9/22/22.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
